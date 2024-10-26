8642be78c74d1a1760ef5ac01b44997e417a4605
dgyatoo <zawngyato@gmail.com>
2024-10-27 00:00:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,26 +8,26 @@ global.GroupWcMsg = false // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
 global.public = true // false ah i dah chuan nang chauhin i hmang thei ang
-global.autoread = true // true a i dah chuan message zawng zawng read vek zelin a awm ang 
-global.autoread_status = true // true a i dah chuan auto status view in a awm ang
+global.autoread = false // true a i dah chuan message zawng zawng read vek zelin a awm ang 
+global.autoread_status = false // true a i dah chuan auto status view in a awm ang
 
 //info
-global.owner = ['918416093656'] // I phone number in thlak rawh
-global.ownernumber = '918416093656' // I phone number in thlak rawh
-global.ownername = "꧁Herbert♕꧂" // I hming dah tel bawk la
+global.owner = ['919863840872'] // I phone number in thlak rawh
+global.ownernumber = '919863840872' // I phone number in thlak rawh
+global.ownername = "Уα𝜏σ ｴട Ԩҽɾҽ" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
-global.botname = "HBWABot Mizo" // I bot hming tur dah la
+global.botname = "yato" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🤖'
-global.creator = "918416093656@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
+global.creator = "919863840872@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
-global.ownerNumber = ["918416093656@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
-global.prefixc = ['/'] // Prefix a tana hman theih loh tur symbolise a awm nuala, i bot a work a lo a nih chuan i thlak mai dawn nia
+global.ownerNumber = ["919863840872@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
+global.prefixc = [''] // Prefix a tana hman theih loh tur symbolise a awm nuala, i bot a work a lo a nih chuan i thlak mai dawn nia
 global.sessionName = 'session'
 
 // Sticker Pack Name
-global.packname = "Sticker By"
-global.author = "HBWABot"
+global.packname = "Sticker By yato"
+global.author = "Уα𝜏σ ｴട Ԩҽɾҽ"
 
 //BUTTON INPUT 
 global.buttonyt = "SUBSCRIBE"
